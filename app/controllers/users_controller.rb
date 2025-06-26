316a092ae75854596ccdc6dbe60b9eb5e5b24245
maryhiguera <marylin0higuera@gmail.com>
2025-06-26 18:17:37 -0500

    adding user crub actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    if @user.save
+      render json: { message: "user created" }
+    else
+      render json: { user.error.full.messages }
+    end
+  end
+
+  def update 
+    @user = User.find(params[:id])
+
+    @user.update(
+      name: params[:name] || @user.name,
+      email: params[:email] || @user.email,
+      password: params[:password] || @user.password,
+      password_confirmation: params[:password_confirmation] || @user.password_confirmation
+    )
+
+    if @user.update 
+      render json: { message: "user updated" }
+    else 
+      render json: { user.error.full.messages }
+    end
+  end
+
+  def destroy 
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: { message: "product destroyed"}
+  end 
 end
